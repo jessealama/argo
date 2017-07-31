2d87485514d134f62f7553ed24589cfe6fb3731e
Jesse Alama <jesse@lisp.sh>
2017-07-31 07:37:37 +0200

    Start to work on format.

---
+++ format.rkt
@@ -0,0 +1,58 @@
+#lang racket/base
+
+;; format validation:
+;;
+;; * date-time
+;; * email
+;; * hostname
+;; * ipv4
+;; * ipv6
+;; * uri
+;; * uri-reference
+;; * uri-template
+;; * json-pointer
+
+(define (date-time? x)
+  #t)
+
+(provide date-time?)
+
+(define (email? x)
+  #t)
+
+(provide email?)
+
+(define (hostname? x)
+  #t)
+
+(provide hostname)
+
+(define (ipv4? x)
+  #t)
+
+(provide ipv4?)
+
+(define (ipv6? x)
+  #t)
+
+(provide ipv6?)
+
+(define (uri? x)
+  #t)
+
+(provide uri?)
+
+(define (uri-reference? x)
+  #t)
+
+(provide uri-reference?)
+
+(define (uri-template? x)
+  #t)
+
+(provide uri-template?)
+
+(define (json-pointer? x)
+  #t)
+
+(provide json-pointer?)
