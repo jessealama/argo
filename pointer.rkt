cbe7ff4cd34345fec67ae25ba8d9fffc6e074dcb
Jesse Alama <jesse@lisp.sh>
2017-08-09 07:07:31 +0200

    Getting started with evaluation.

--- pointer.rkt
+++ pointer.rkt
@@ -3,6 +3,12 @@
 (module+ test
   (require rackunit))
 
+(require (only-in json
+                  jsexpr?))
+
+(require (only-in (file "json.rkt")
+                  json-object?))
+
 (require br-parser-tools/lex)
 (require (only-in brag/support
                   token
@@ -41,12 +47,15 @@
 
 (provide make-tokenizer)
 
+(define (parse-json-pointer s)
+  (parse (make-tokenizer (open-input-string s))))
+
 (define (json-pointer? x)
   (and (string? x)
        (with-handlers ([exn:fail:parsing? (lambda (e) #f)])
          (begin0
              #t
-           (parse (make-tokenizer (open-input-string x)))))))
+           (parse-json-pointer x)))))
 
 (module+ test
   (check-false (json-pointer? " "))
@@ -78,3 +87,86 @@
   (check-true (json-pointer/uri-fragment? "#/definitions/foo"))
   (check-true (json-pointer/uri-fragment? (string->url "http://schema.foo.com/main#/definitions/nice")))
   (check-false (json-pointer/uri-fragment? "/definitions/foo")))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Evaluation
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; json-pointer? jsexpr?
+(define (pointer-value jp doc)
+  (unless (json-pointer? jp)
+    (error "Not a JSON Pointer." jp))
+  (unless (jsexpr? doc)
+    (error "Not a JSON value."))
+  (unless (json-object? doc)
+    (error "Not a JSON object."))
+  doc)
+
+(module+ test
+  (define sample-doc/str #<<SAMPLE
+ {
+      "foo": ["bar", "baz"],
+      "": 0,
+      "a/b": 1,
+      "c%d": 2,
+      "e^f": 3,
+      "g|h": 4,
+      "i\\j": 5,
+      "k\"l": 6,
+      " ": 7,
+      "m~n": 8
+   }
+SAMPLE
+    ))
+
+(module+ test
+  (define-values (sample-doc/jsexpr sample-ok?)
+    (parse-json-string sample-doc/str))
+  (check-true sample-ok?)
+  (require (only-in (file "parse.rkt")
+                    parse-json-string))
+  (require (only-in (file "json.rkt")
+                    json-equal?
+                    json-string
+                    json-number
+                    json-array))
+  (check json-equal?
+         (pointer-value "" sample-doc/jsexpr)
+         sample-doc/jsexpr)
+  (check json-equal?
+         (pointer-value "/foo" sample-doc/jsexpr)
+         (json-array (json-string "bar")
+                     (json-string "baz")))
+  (check json-equal?
+         (pointer-value "/foo/0" sample-doc/jsexpr)
+         (json-string "bar"))
+  (check json-equal?
+         (pointer-value "/" sample-doc/jsexpr)
+         (json-number 0))
+  (check json-equal?
+         (pointer-value "/a~1b" sample-doc/jsexpr)
+         (json-number 1))
+  (check json-equal?
+         (pointer-value "/c%d" sample-doc/jsexpr)
+         (json-number 2))
+  (check json-equal?
+         (pointer-value "/e^f" sample-doc/jsexpr)
+         (json-number 3))
+  (check json-equal?
+         (pointer-value "/g|h" sample-doc/jsexpr)
+         (json-number 4))
+  (check json-equal?
+         (pointer-value "/i\\\\j" sample-doc/jsexpr)
+         (json-number 5))
+  (check json-equal?
+         (pointer-value "/i\\\\j" sample-doc/jsexpr)
+         (json-number 5))
+  (check json-equal?
+         (pointer-value "k\"l" sample-doc/jsexpr)
+         (json-number 6))
+  (check json-equal?
+         (pointer-value "/ " sample-doc/jsexpr)
+         (json-number 7))
+  (check json-equal?
+         (pointer-value "m~0n" sample-doc/jsexpr)
+         (json-number 8)))
