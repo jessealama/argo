ea8ac79d212c3e51d1e94962338242fe1ea49453
Jesse Alama <jesse@lisp.sh>
2017-08-10 07:08:18 +0200

    More work and tests.

--- resolve.rkt
+++ resolve.rkt
@@ -58,33 +58,51 @@
                   (resolve-ref-wrt-id "person.json"
                                       url/string))))
 
-; string? string?|#f jsexpr? -> jsexpr?|#f boolean?
+; string?|url? string?|#f jsexpr? -> jsexpr? boolean?
 (define (resolve-schema-wrt-id ref id document)
   (cond ((string? id)
-         (let ([id/url (string->url id)])
-           (let ([combined (combine-url/relative id/url ref)])
-             (resolve-schema-wrt-id combined #f document))))
-        ((json-pointer? ref)
-         (cond ((jsexpr? document)
-                (resolve-pointer-in-document ref document))
+         (let* ([id/url (string->url id)]
+                [combined (combine-url/relative id/url ref)])
+           (resolve-schema-wrt-id combined #f document)))
+        ((eq? id #f)
+         (cond ((string? ref)
+                (resolve-schema-wrt-id (string->url ref) id document))
+               ((url? ref)
+                (define fragment (url-fragment ref))
+                (when (string? fragment)
+                  (unless (json-pointer? fragment)
+                    (error "Fragment part of URL is not a JSON Pointer:" fragment)))
+                (cond ((url-has-only-fragment? ref)
+                       (values (pointer-value fragment document)
+                               #t))
+                      ((string? (url-host ref))
+                       (define url-w/o-fragment
+                         (struct-copy url
+                                      ref
+                                      [fragment #f]))
+                       (define url-w/o-fragment/str (url->string url-w/o-fragment))
+                       (define-values (path header)
+                         (uri&headers->path&header url-w/o-fragment/str (list)))
+                       (define-values (in out)
+                         (connect-uri ref))
+                       (define ok? (start-request in
+                                                  out
+                                                  "1.1"
+                                                  "GET"
+                                                  path
+                                                  header))
+                       (define h (purify-port/log-debug in))
+                       (define schema/bytes (read-entity/bytes in h))
+                       (define-values (schema well-formed?)
+                         (parse-json-bytes schema/bytes))
+                       (cond ((not well-formed?)
+                              (values #f #f))
+                             ((string? fragment)
+                              (values (pointer-value fragment schema) #f))
+                             (else
+                              (values schema #t))))))
                (else
-                (values #f #f))))
-        (else
-         (define url (string->url ref))
-         (define-values (path header) (uri&headers->path&header url (list)))
-         (define-values (in out) (connect-uri url))
-         (define ok? (start-request in
-                                    out
-                                    "1.1"
-                                    "GET"
-                                    path
-                                    header))
-         (define h (purify-port/log-debug in))
-         (define schema/bytes (read-entity/bytes in h))
-         (define-values (schema well-formed?) (parse-json-bytes schema/bytes))
-         (if well-formed?
-             (values schema #t)
-             (values #f #f)))))
+                (error "ref should be either a string or a URL." ref))))))
 
 (provide resolve-schema-wrt-id)
 
@@ -100,8 +118,72 @@
     }
 }
 SCHEMA
+  ))
+
+(module+ test
+  (require (only-in (file "parse.rkt")
+                    parse-json-string))
+  (require (only-in (file "json.rkt")
+                    json-equal?))
+  (define-values (geo/jsexp geo-ok?)
+    (parse-json-string geo/str))
+  (check-true geo-ok?)
+  ;; (define-values (resolved-geo resolved-ok?)
+  ;;   (resolve-schema-wrt-id "http://json-schema.org/geo" #f (hasheq)))
+  ;; (check-true resolved-ok?)
+  ;; (check-true (json-equal? resolved-geo geo/jsexp))
   )
-  (define geo/jsexp (parse-json-string geo/str))
-  (check json-equal?
-         (resolve-schema-wrt-id "http://json-schema.org/geo" #f (hasheq))
-         geo/jsexp))
+
+(module+ test
+  ;; https://spacetelescope.github.io/understanding-json-schema/structuring.html
+  (define address-schema/str #<<ADDRESS_SCHEMA
+{
+    "type": "object",
+    "properties": {
+        "street_address": { "type": "string" },
+        "city":           { "type": "string" },
+        "state":          { "type": "string" }
+    },
+    "required": ["street_address", "city", "state"]
+}
+ADDRESS_SCHEMA
+))
+
+(module+ test
+  (define full-address-schema/str #<<FULL_SCHEMA
+{
+  "$schema": "http://json-schema.org/draft-04/schema#",
+
+  "definitions": {
+    "address": {
+      "type": "object",
+      "properties": {
+        "street_address": { "type": "string" },
+        "city":           { "type": "string" },
+        "state":          { "type": "string" }
+      },
+      "required": ["street_address", "city", "state"]
+    }
+  },
+
+  "type": "object",
+
+  "properties": {
+    "billing_address": { "$ref": "#/definitions/address" },
+    "shipping_address": { "$ref": "#/definitions/address" }
+  }
+}
+FULL_SCHEMA
+))
+
+(module+ test
+  (define-values (address/jsexpr address-ok?)
+    (parse-json-string address-schema/str))
+  (check-true address-ok?)
+  (define-values (full-schema/jsexpr full-schema-ok?)
+    (parse-json-string full-address-schema/str))
+  (check-true full-schema-ok?)
+  (define-values (resolved-value resolved?)
+    (resolve-schema-wrt-id "#/definitions/address" #f full-schema/jsexpr))
+  (check-true resolved?)
+  (check-true (json-equal? resolved-value address/jsexpr)))
