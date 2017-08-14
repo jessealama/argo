15eda893a87264749f344329f596fcbec4bc147d
Jesse Alama <jesse@lisp.sh>
2017-08-14 21:51:11 +0200

    Adjusting the lexer and parser.

--- uri-template.rkt
+++ uri-template.rkt
@@ -3,6 +3,7 @@
 (module+ test
   (require rackunit))
 
+(require net/url-structs)
 (require br-parser-tools/lex)
 (require (only-in net/url-string
                   string->url))
@@ -23,11 +24,13 @@
    [(eof)
     eof]
    ["{"
-    (token 'CURLY-OPEN "{")]
+    (token "{" "{")]
    ["}"
-    (token 'CURLY-CLOSE "}")]
-   [#\~
-    (token 'TILDE "~")]
+    (token "}" "}")]
+   [(char-range "0" "9")
+    (token 'NUMERAL lexeme)]
+   [(char-range "A" "z")
+    (token 'LETTER lexeme)]
    [(union #\!                          ; %x21
            (char-range #\# #\$)         ; %x23-24
            #\&                          ; %x26
@@ -37,7 +40,7 @@
            "]"                          ; %x5D
            #\_                          ; %x5F
            (char-range #\a #\z)         ; %x61-7A
-           #\|                          ; %x7E
+           #\~                          ; %x7E
 
            ;; ucschar
            (char-range #\u00a0 #\ud7ff)
@@ -70,7 +73,9 @@
 (module+ test
   (check-not-exn (lambda () (lex-uri-template "http://example.com/~{username}/")))
   (check-not-exn (lambda () (lex-uri-template "http://example.com/dictionary/{term:1}/{term}")))
-  (check-not-exn (lambda () (lex-uri-template "http://example.com/search{?q,lang}"))))
+  (check-not-exn (lambda () (lex-uri-template "http://example.com/search{?q,lang}")))
+  (check-not-exn (lambda () (lex-uri-template "http://www.example.com/foo{?query,number}")))
+  (check-not-exn (lambda () (lex-uri-template "http://www.example.com/foo{?query,number}"))))
 
 (define (make-tokenizer port)
   (define (next-token)
@@ -89,6 +94,36 @@
 
 (provide uri-template?)
 
+(module+ test
+  (check-true (uri-template? "http://example.com/~{username}/"))
+  (check-true (uri-template? "http://example.com/dictionary/{term:1}/{term}"))
+  (check-true (uri-template? "http://example.com/search{?q,lang}"))
+  (check-true (uri-template? "http://www.example.com/foo{?query,number}"))
+  (check-true (uri-template? "http://www.example.com/foo{?query,number}")))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Parameters
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define (uri-template-parameter-pair? x)
+  (and (pair? x)
+       (symbol? (car x))))
+
+(define (uri-template-parameters? x)
+  (and (list? x)
+       (andmap uri-template-parameter-pair? x)))
+
+(define (make-uri-template-parameters . more)
+  (cond ((null? more)
+         (list))
+        ((null? (cdr more))
+         (error "No more arguments after:" (car more)))
+        (else
+         (let ([p (cons (car more) (cdr more))])
+           (unless (uri-template-parameter-pair? p)
+             (error "Not a URI template parameter pair:" p))
+           (cons p (make-uri-template-parameters (rest (rest more))))))))
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Evaluation
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -98,3 +133,42 @@
   (string->url "http://racket-lang.org"))
 
 (provide expand-uri-template)
+
+(module+ test
+  (let* ([template "http://www.example.com/foo{?query,number}"]
+         [parameters (list (cons 'query "mycelium")
+                           (cons 'number 100))])
+    (test-begin
+      (check-true (uri-template? template))
+      (check-true (uri-template-parameters? parameters))
+      (let ([expanded (expand-uri-template template parameters)])
+        (test-begin
+          (check-true (url? expanded))
+          (check-true (string=? "http"
+                                (url-scheme expanded)))
+          (check-eq? #f
+                     (url-user expanded))
+          (check-true (string=? "www.example.com"
+                                (url-host expanded)))
+          (check-eq? #f
+                     (url-port expanded))
+          (check-eq? #t
+                     (url-path-absolute? expanded))
+          (check-= 1 (length (url-path expanded)) 0)
+          (check-true (string=? "foo"
+                                (first (url-path expanded))))
+          (check-true #f
+                      (url-fragment expanded))
+          (let ([query (url-query expanded)])
+            (test-begin
+              (check-= 2 (length query) 0)
+              (let ([q (first query)])
+                (test-begin
+                  (check-eq? 'query (car q))
+                  (check-true (string? (cdr q)))
+                  (check-true (string=? "mycelium" (cdr q)))))
+              (let ([q (second query)])
+                (test-begin
+                  (check-eq? 'number (car q))
+                  (check-true (string? (cdr q)))
+                  (check-true (string=? "100" (cdr q))))))))))))
