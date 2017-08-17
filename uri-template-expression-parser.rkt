b3777aee79f2ecc3a494328e9f0184b82f64c4bc
Jesse Alama <jesse@lisp.sh>
2017-08-17 06:44:02 +0200

    Parsing template expressions, not arbitrary strings.

--- uri-template-expression-parser.rkt
+++ uri-template-expression-parser.rkt
@@ -1,9 +1,5 @@
 #lang brag
-uri-template : (literals | expression)*
-literals: LETTER | DIGIT | "." | ":" | "?" | SAFE-CHAR |  pct-encoded
-pct-encoded: "%" hex-digit
-hex-digit: DIGIT | hexchar
-hexchar: "A" | "B" | "C" | "D" | "E" | "F" | "a" | "b" | "c" | "d" | "e" | "f"
+; We parse only URI Template expressions, not full URI Templates
 expression: "{" [ operator ] variable-list "}"
 operator: op-level2 | op-level3 | op-reserve
 op-level2: "+" | "#"
@@ -12,8 +8,8 @@ op-reserve: "=" | "," | "!" | "@" | "|"
 variable-list: varspec ( "," varspec)*
 varspec: varname [ modifier-level4 ]
 varname: varchar ([ "." ] varchar)*
-varchar: LETTER | "_" | pct-encoded
+varchar: LETTER | "_" | PCT-ENCODED
 modifier-level4: prefix | explode
 prefix: ":" max-length
-max-length: NON-ZERO-DIGIT ( DIGIT )*
+max-length: NUMBER
 explode: "*"
