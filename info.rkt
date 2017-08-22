b254e6973d3fce889c4134296da1888b95ecf3af
Jesse Alama <jesse@lisp.sh>
2017-08-22 06:30:25 +0200

    Playing around with info.rkt.

--- info.rkt
+++ info.rkt
@@ -1,9 +1,10 @@
 #lang info
 (define collection "argo")
 (define deps '("base"
-               "rackunit-lib"))
-(define build-deps '("scribble-lib" "racket-doc"))
+               "rackunit-lib"
+	       "mutt"))
+(define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
 (define scribblings '(("scribblings/argo.scrbl" ())))
-(define pkg-desc "Description Here")
-(define version "0.0")
-(define pkg-authors '(jesse))
+(define pkg-desc "Argo is a JSON Schema validator.")
+(define version "0.1")
+(define pkg-authors '("jesse@lisp.sh"))
