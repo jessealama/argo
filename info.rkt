6fd7aa9d819fe2bebc23e1126a12e0e0a144f5ba
Jesse Alama <jesse@lisp.sh>
2017-08-26 06:47:41 +0200

    What do we need?

--- info.rkt
+++ info.rkt
@@ -1,8 +1,12 @@
 #lang info
 (define collection "argo")
 (define deps '("base"
-               "rackunit-lib"))
-(define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
+               "rackunit-lib"
+	       "http"
+	       "brag"
+	       "sugar"
+	       "br-parser-tools-lib"))
+(define build-deps '("scribble-lib" "racket-doc" "rackunit-lib" "beautiful-racket-lib"))
 (define scribblings '(("scribblings/argo.scrbl" ())))
 (define pkg-desc "Argo is a JSON Schema validator.")
 (define version "0.1")
