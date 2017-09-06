72d3d26243237a9a2e28a76d45f22d6ad53d3cbf
Jesse Alama <jesse@lisp.sh>
2017-09-06 06:53:12 +0200

    More exports.

--- main.rkt
+++ main.rkt
@@ -9,7 +9,16 @@
 (require (only-in (file "validate.rkt")
                   adheres-to-schema?
                   check-json/schema))
+(require (only-in (file "json.rkt")
+                  json-equal?))
+(require (only-in (file "pointer.rkt")
+                  json-pointer-value))
+(require (only-in (file "pp.rkt")
+                  json-pretty-print))
 
 (provide adheres-to-schema?
          json-schema?
-         check-json/schema)
+         check-json/schema
+         json-equal?
+         json-pretty-print
+         json-pointer-value)
