9ce6e64454de4976e58a7d40e8d717877572d2d9
Jesse Alama <jesse@lisp.sh>
2017-09-13 07:38:53 +0200

    Drop unused code.

--- resolve.rkt
+++ resolve.rkt
@@ -9,8 +9,6 @@
                     parse-json-string
                     parse-json-url))
 
-;; (require http/request)
-
 (require (only-in (file "parameters.rkt")
                   original-schema
                   current-id))
@@ -84,19 +82,6 @@
                          (struct-copy url
                                       ref
                                       [fragment #f]))
-                       ;; (define url-w/o-fragment/str (url->string url-w/o-fragment))
-                       ;; (define-values (path header)
-                       ;;   (uri&headers->path&header url-w/o-fragment/str (list)))
-                       ;; (define-values (in out)
-                       ;;   (connect-uri url-w/o-fragment/str))
-                       ;; (define ok? (start-request in
-                       ;;                            out
-                       ;;                            "1.1"
-                       ;;                            "GET"
-                       ;;                            path
-                       ;;                            header))
-                       ; (define h (purify-port/log-debug in))
-                       ; (define schema/bytes (read-entity/bytes in h))
                        (define-values (schema well-formed?)
                          (parse-json-url url-w/o-fragment))
                        (cond ((not well-formed?)
